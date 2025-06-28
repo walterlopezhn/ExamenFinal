20d17ed6f3104768a801d305e0a7cc03bcb28c33
Darking01 <enrique.fuentes@unitec.edu>
2025-06-28 14:29:06 -0600

    conexion con firebase
    dependencias y mas

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
 //brrrrrrrrr
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
